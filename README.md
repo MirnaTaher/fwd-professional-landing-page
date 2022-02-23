f92bdf1b0e7c0e988aefb53dd880602d5ecc8c81
unknown <Mirna Taher>
2022-02-23 07:15:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Landing Page Project
+
+## Table of Contents
+this project has 5 sections
+it contains the following files:
+index.html
+styles.css
+app.js
+
+most of the modification are in the javascript file, and some edits to the css file and I added two sections to the end of the html file just to make it more clear that the nav menu works with variable numbers of sections.
+
+the app.js file contains the following:
+-some variables like sections htmlCollection and navbar list htmlCollection
+-then we have some event listener, one for clicking on the navbar anchors and another one for scrolling
+-and one helper function to see whether or not the section is in viewport
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
---
+++ js/app.js
@@ -0,0 +1,86 @@
+//global variables
+// const sections = document.getElementsByTagName("section");
+let sections = document.querySelectorAll("section");
+const nav = document.getElementById("navbar__list");
+let navbar__items = nav.getElementsByTagName("li");
+let id = "section1";
+
+//creating a fragment of navbar list
+//making a li element and inside it an anchor element to link it to the corresponding section
+let liFragment = document.createDocumentFragment();
+for (let section of sections) {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.innerText = section.getAttribute("data-nav");
+  a.setAttribute("active", false);
+  a.setAttribute("href", `#${section.getAttribute("id")}`);
+  li.addEventListener("click", addActiveClass);
+  li.appendChild(a);
+  liFragment.appendChild(li);
+}
+nav.appendChild(liFragment);
+
+//event listener functions
+
+//when we click on navbar items
+//first we remove the state active from all navbar items then we added it to the one we just clicked
+//we also remove the "your-active-class" from all sections then we added to the one linked to the ancor we clicked on
+function addActiveClass() {
+  for (let item of navbar__items) {
+    item.getElementsByTagName("a")[0].setAttribute("active", false);
+    document
+      .querySelector(
+        `${item.getElementsByTagName("a")[0].getAttribute("href")}`
+      )
+      .classList.remove("your-active-class");
+  }
+  this.getElementsByTagName("a")[0].setAttribute("active", true);
+  document
+    .querySelector(`${this.getElementsByTagName("a")[0].getAttribute("href")}`)
+    .classList.add("your-active-class");
+}
+
+//changing which section is highlighted when scrolling and the corresponding
+//nav anchor as well
+document.addEventListener("scroll", function (e) {
+  // let ids=[];
+
+  for (let section of sections) {
+    if (elementInViewport2(section)) {
+      //there could be more than one section in viewport, so we push the id to the array
+      //so that we highlight the first one
+      // ids.push(section.getAttribute("id"))
+      id = section.getAttribute("id");
+    }
+    //removing previous styling
+    for (let item of navbar__items) {
+      item.getElementsByTagName("a")[0].setAttribute("active", false);
+      document
+        .querySelector(
+          `${item.getElementsByTagName("a")[0].getAttribute("href")}`
+        )
+        .classList.remove("your-active-class");
+      if (item.getElementsByTagName("a")[0].getAttribute("href") == `#${id}`) {
+        item.getElementsByTagName("a")[0].setAttribute("active", true);
+      }
+    }
+
+    //section highlight
+    document.getElementById(id).classList.add("your-active-class");
+    //navbar anchor highlight
+    // console.log(document.querySelectorAll(`[href=${id}]`));
+  }
+});
+
+//helper functions
+
+//function to check if the element passed to it is in viewport or not
+function elementInViewport2(el) {
+  var sectionTop = el.getBoundingClientRect().top;
+  var sectionBottom = el.getBoundingClientRect().bottom;
+  if (sectionTop <= 60 && sectionBottom  >60) {
+    return true;
+  } else {
+    return false;
+  }
+}
